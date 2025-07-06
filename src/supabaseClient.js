2aa0b01ba546d833ad6213b83fb91555e68f1188
Charles Dennis <dencharles01@gmail.com>
2025-07-06 06:19:59 +0100

    Fix Supabase env var name to REACT_APP_SUPABASE_KEY

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON
+  process.env.REACT_APP_SUPABASE_KEY
 );
